c65eb9a05029c08a8ff9b504a1c4910ac399eb51
Joel Scoble <joel.scoble@outlook.com>
2015-12-17 19:10:42 +0000

    fmtd

--- varint.go
+++ varint.go
@@ -32,10 +32,10 @@ func Decode(buf []byte) (uint64, int) {
 		return 2288 + 256*uint64(buf[1]) + uint64(buf[2]), 3
 	}
 	if b == 0xFA {
-		return  uint64(buf[1])<<16 | uint64(buf[2])<<8 | uint64(buf[3]), 4
+		return uint64(buf[1])<<16 | uint64(buf[2])<<8 | uint64(buf[3]), 4
 	}
 	if b == 0xFB {
-		return  uint64(buf[1])<<24 | uint64(buf[2])<<16 | uint64(buf[3])<<8 | uint64(buf[4]), 5
+		return uint64(buf[1])<<24 | uint64(buf[2])<<16 | uint64(buf[3])<<8 | uint64(buf[4]), 5
 	}
 	if b == 0xFC {
 		return uint64(buf[1])<<32 | uint64(buf[2])<<24 | uint64(buf[3])<<16 | uint64(buf[4])<<8 | uint64(buf[5]), 6
@@ -84,57 +84,57 @@ func Encode(buf []byte, x uint64) int {
 	}
 	if x < 1<<24 {
 		buf[0] = 0xFA
-		buf[1] = byte(x>>16)
-		buf[2] = byte(x>>8)
+		buf[1] = byte(x >> 16)
+		buf[2] = byte(x >> 8)
 		buf[3] = byte(x)
 		return 4
 	}
 	if x < 1<<32 {
 		buf[0] = 0xFB
-		buf[1] = byte(x>>24)
-		buf[2] = byte(x>>16)
-		buf[3] = byte(x>>8)
+		buf[1] = byte(x >> 24)
+		buf[2] = byte(x >> 16)
+		buf[3] = byte(x >> 8)
 		buf[4] = byte(x)
 		return 5
 	}
 	if x < 1<<40 {
 		buf[0] = 0xFC
-		buf[1] = byte(x>>32)
-		buf[2] = byte(x>>24)
-		buf[3] = byte(x>>16)
-		buf[4] = byte(x>>8)
+		buf[1] = byte(x >> 32)
+		buf[2] = byte(x >> 24)
+		buf[3] = byte(x >> 16)
+		buf[4] = byte(x >> 8)
 		buf[5] = byte(x)
 		return 6
 	}
 	if x < 1<<48 {
 		buf[0] = 0xFD
-		buf[1] = byte(x>>40)
-		buf[2] = byte(x>>32)
-		buf[3] = byte(x>>24)
-		buf[4] = byte(x>>16)
-		buf[5] = byte(x>>8)
+		buf[1] = byte(x >> 40)
+		buf[2] = byte(x >> 32)
+		buf[3] = byte(x >> 24)
+		buf[4] = byte(x >> 16)
+		buf[5] = byte(x >> 8)
 		buf[6] = byte(x)
 		return 7
 	}
 	if x < 1<<56 {
 		buf[0] = 0xFE
-		buf[1] = byte(x>>48)
-		buf[2] = byte(x>>40)
-		buf[3] = byte(x>>32)
-		buf[4] = byte(x>>24)
-		buf[5] = byte(x>>16)
-		buf[6] = byte(x>>8)
+		buf[1] = byte(x >> 48)
+		buf[2] = byte(x >> 40)
+		buf[3] = byte(x >> 32)
+		buf[4] = byte(x >> 24)
+		buf[5] = byte(x >> 16)
+		buf[6] = byte(x >> 8)
 		buf[7] = byte(x)
 		return 8
 	}
 	buf[0] = 0xFF
-	buf[1] = byte(x>>56)
-	buf[2] = byte(x>>48)
-	buf[3] = byte(x>>40)
-	buf[4] = byte(x>>32)
-	buf[5] = byte(x>>24)
-	buf[6] = byte(x>>16)
-	buf[7] = byte(x>>8)
+	buf[1] = byte(x >> 56)
+	buf[2] = byte(x >> 48)
+	buf[3] = byte(x >> 40)
+	buf[4] = byte(x >> 32)
+	buf[5] = byte(x >> 24)
+	buf[6] = byte(x >> 16)
+	buf[7] = byte(x >> 8)
 	buf[8] = byte(x)
 	return 9
 }
